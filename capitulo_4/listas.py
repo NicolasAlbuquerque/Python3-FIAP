5e053adc4aad6c49dfb72c7d1114c066be439d61
Nicolas Albuquerque <contato.nicolasalbuquerque@gmail.com>
2025-09-27 17:22:26 -0300

    Capitulo 3

---
+++ capitulo_4/listas.py
@@ -0,0 +1,16 @@
+#CRIAR LISTA VAZIA
+jedi = []
+print(type(jedi))
+#CRIAÇÃO DE UMA LISTA COM OS JEDI
+jedi= ['Luke', 'Yoda','Obi-Wan', 'Anakin']
+#EXIBIÇÃO DE UMA LISTA COMPLETA
+print('versão um ', jedi)
+#EXIBIÇÃO DE UMA POSIÇÃO ESPECIFICA NA LISTA
+print(jedi[0])
+#EXIBIÇÃO DO ÚLTIMO ELEMNTO
+print(jedi[-1])
+print(jedi[1:3]) #entre o 1 e o 3
+#exibição de cada item da lista
+for x in jedi:
+    print(x)
+nomes = ['Nicolas', 'Horacio', 'Teobaldo']
